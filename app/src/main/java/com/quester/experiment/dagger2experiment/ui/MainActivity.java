dc98c29b8083af0d928d770d37302b16479a8052
Antolis <josipantolis@gmail.com>
2015-01-16 22:26:23 +0100

    implemented simple MainActivity, used butterknife fo slice it even thinner

--- app/src/main/java/com/quester/experiment/dagger2experiment/ui/MainActivity.java
+++ app/src/main/java/com/quester/experiment/dagger2experiment/ui/MainActivity.java
@@ -1,36 +1,36 @@
 package com.quester.experiment.dagger2experiment.ui;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Parcelable;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.quester.experiment.dagger2experiment.ApplicationComponent;
 import com.quester.experiment.dagger2experiment.InjectionActivity;
 import com.quester.experiment.dagger2experiment.R;
-import com.quester.experiment.dagger2experiment.data.area.Circle;
-import com.quester.experiment.dagger2experiment.data.area.CircularArea;
-import com.quester.experiment.dagger2experiment.data.area.Point;
-import com.quester.experiment.dagger2experiment.data.checkpoint.Checkpoint;
+import com.quester.experiment.dagger2experiment.data.MockedQuestUtils;
+import com.quester.experiment.dagger2experiment.data.quest.Quest;
+import com.quester.experiment.dagger2experiment.engine.GameEngineService;
 
 import org.parceler.Parcels;
 
 import javax.inject.Inject;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
-public class MainActivity extends InjectionActivity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+
+public class MainActivity extends InjectionActivity {
+
+    private Parcelable wrappedQuest;
+
+    @InjectView(R.id.text_view)
+    TextView infoTextVeiw;
 
     private static final String TAG = "MainActivity";
 
@@ -39,16 +39,6 @@ public class MainActivity extends InjectionActivity
      */
     @Inject
     protected LocationManager locationManager;
-
-    /**
-     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
-     */
-    private NavigationDrawerFragment mNavigationDrawerFragment;
-
-    /**
-     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
-     */
-    private CharSequence mTitle;
 
     /**
      * "Default" implementation of @see InjectionActivity#inject
@@ -74,131 +64,32 @@ public class MainActivity extends InjectionActivity
 
         setContentView(R.layout.activity_main);
 
-        mNavigationDrawerFragment = (NavigationDrawerFragment)
-                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
-        mTitle = getTitle();
+        ButterKnife.inject(this);
 
-        // Set up the drawer.
-        mNavigationDrawerFragment.setUp(
-                R.id.navigation_drawer,
-                (DrawerLayout) findViewById(R.id.drawer_layout));
+        Quest mockedQuest = MockedQuestUtils.getMockedLinearQuest(1, "Mocked linear quest", 3);
+        wrappedQuest = Parcels.wrap(mockedQuest);
+
+        infoTextVeiw.setText(mockedQuest.toString());
     }
 
-    @Override
-    public void onNavigationDrawerItemSelected(int position) {
-        // update the main content by replacing fragments
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
-                .commit();
+    @OnClick(R.id.start_button)
+    public void starService() {
+        startService(new Intent(this, GameEngineService.class).putExtra(GameEngineService.QUEST_EXTRA_ID, wrappedQuest));
     }
 
-    public void onSectionAttached(int number) {
-        switch (number) {
-            case 1:
-                mTitle = getString(R.string.title_section1);
-                break;
-            case 2:
-                mTitle = getString(R.string.title_section2);
-                break;
-            case 3:
-                mTitle = getString(R.string.title_section3);
-                break;
-        }
+    @OnClick(R.id.stop_button)
+    public void stoService() {
+        stopService(new Intent(this, GameEngineService.class));
     }
-
-    public void restoreActionBar() {
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-        actionBar.setDisplayShowTitleEnabled(true);
-        actionBar.setTitle(mTitle);
-    }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        if (!mNavigationDrawerFragment.isDrawerOpen()) {
-            // Only show items in the action bar relevant to this screen
-            // if the drawer is not showing. Otherwise, let the drawer
-            // decide what to show in the action bar.
-            getMenuInflater().inflate(R.menu.main, menu);
-            restoreActionBar();
-            return true;
-        }
-        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        return item.getItemId() == R.id.action_settings || super.onOptionsItemSelected(item);
     }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            //create a Checkpoint, fill it with data, wrap it in parcel, unwrap and print onscreen:
-            Checkpoint checkpoint = new Checkpoint();
-
-            checkpoint.setId(1L);
-            checkpoint.setArea(new CircularArea(new Circle(new Point(1.0, 2.0), 3.0), 1L));
-            checkpoint.setName("Checkpoint 1");
-            checkpoint.setRoot(true);
-            checkpoint.setViewHtmlFileName("path.html");
-            checkpoint.setEventsScriptFileName("path1.js");
-
-            Parcelable wrapped = Parcels.wrap(Checkpoint.class, checkpoint);
-
-            Checkpoint unwrapped = Parcels.unwrap(wrapped);
-
-            TextView textView = (TextView) rootView.findViewById(R.id.textView);
-            textView.setText(unwrapped.toString());
-
-            return rootView;
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((MainActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }
-    }
-
 }
