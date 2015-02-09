85ac520079374ae7fdc257755f4f31e94971a69a
tdubravcevic <tdubravcevic@infobip.com>
2015-02-09 13:17:08 +0100

    added flyway and db migration

--- app/src/main/java/com/quester/experiment/dagger2experiment/persistence/module/DatabaseModule.java
+++ app/src/main/java/com/quester/experiment/dagger2experiment/persistence/module/DatabaseModule.java
@@ -1,10 +1,13 @@
 package com.quester.experiment.dagger2experiment.persistence.module;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
 import com.quester.experiment.dagger2experiment.InjectionApplication;
 import com.quester.experiment.dagger2experiment.persistence.QuestRepository;
 import com.quester.experiment.dagger2experiment.persistence.wrapper.Database;
+
+import org.flywaydb.core.Flyway;
 
 import javax.inject.Singleton;
 
@@ -17,7 +20,14 @@ public class DatabaseModule {
     private final Database database;
 
     public DatabaseModule(InjectionApplication application) {
-        this.database = new Database(application.openOrCreateDatabase("test", 0, null));
+
+        SQLiteDatabase sqLiteDatabase = application.openOrCreateDatabase("test", 0, null);
+
+        Flyway flyway = new Flyway();
+        flyway.setDataSource("jdbc:sqlite:" + sqLiteDatabase.getPath(), "", "");
+        flyway.migrate();
+
+        this.database = new Database(sqLiteDatabase);
     }
 
     @Provides
